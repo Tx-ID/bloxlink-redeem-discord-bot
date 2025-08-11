1bb209b7327321dd67c04c648bfa29e089b9744b
Tix <tix.personal@gmail.com>
2025-08-12 00:31:34 +0700

    config

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,20 @@
-import * as z from "zod"; 
+import * as z from "zod";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default {
 
-    DISCORD_BOT_TOKEN: z.coerce.string().nullable().parse(process.env.DISCORD_BOT_TOKEN),
+    DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN ? z.coerce.string().nullable().parse(process.env.DISCORD_BOT_TOKEN) : null,
 
-    DEADLINE_ACTIVE: z.coerce.boolean().default(false).parse(process.env.DEADLINE_ACTIVE),
-    DEADLINE_UNIX: z.coerce.date().nullable().parse(process.env.DEADLINE_UNIX),
+    DEADLINE_ACTIVE: z.coerce.boolean({error: "Invalid type for: DEADLINE_ACTIVE"}).default(false).parse(process.env.DEADLINE_ACTIVE),
+    DEADLINE_UNIX: process.env.DEADLINE_UNIX ? z.coerce.date({error: "Invalid type for: DEADLINE_UNIX"}).nullable().parse(process.env.DEADLINE_UNIX) : null,
 
     DB_FILENAME: z.coerce.string().default("db.json").parse(process.env.DB_FILENAME),
     CODES_FILENAME: z.coerce.string().default("codes.txt").parse(process.env.CODES_FILENAME),
-    ROBLOX_BADGE_ID: z.coerce.number().int().nullable().parse(process.env.ROBLOX_BADGE_ID),
+    ROBLOX_BADGE_ID: process.env.ROBLOX_BADGE_ID ? z.coerce.number({error: "Invalid type for: ROBLOX_BADGE_ID"}).nullable().parse(process.env.ROBLOX_BADGE_ID) : null,
 
-    BLOXLINK_API_KEY: z.coerce.string().nullable().parse(process.env.BLOXLINK_API_KEY),
+    BLOXLINK_API_KEY: process.env.BLOXLINK_API_KEY ? z.coerce.string().nullable().parse(process.env.BLOXLINK_API_KEY) : null,
 
     BLOXLINK_API_BASE_URL: z.coerce.string().default("https://api.blox.link/v4/").parse(process.env.BLOXLINK_API_BASE_URL),
-    // ROBLOX_API_BASE_URL: z.coerce.string().default("https://roproxy.com").parse(process.env.ROBLOX_API_BASE_URL),
     
 };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as DiscordJs from 'discord.js';
 import * as Axios from 'axios';
 
-import dotenv from 'dotenv';
-dotenv.config();
+import config from './config';
+console.log(config);
