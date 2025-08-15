862ec76e828cc5f10431eba91b4d165a8681797c
Tix <tix.personal@gmail.com>
2025-08-15 23:22:50 +0700

    tim hore

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import moment from 'moment-timezone';
+
 import * as z from "zod";
 import dotenv from 'dotenv';
 dotenv.config();
@@ -10,11 +12,11 @@ export default {
     DEADLINE_UNIX: process.env.DEADLINE_UNIX ? z.coerce.date({error: "Invalid type for: DEADLINE_UNIX"}).nullable().parse(process.env.DEADLINE_UNIX) : null,
 
     DB_FILENAME: z.coerce.string().default("db.json").parse(process.env.DB_FILENAME),
-    CODES_FILENAME: z.coerce.string().default("codes.txt").parse(process.env.CODES_FILENAME),
+    CODES_FOLDERNAME: z.coerce.string().default("codes").parse(process.env.CODES_FOLDERNAME),
+    
     ROBLOX_BADGE_ID: process.env.ROBLOX_BADGE_ID ? z.coerce.number({error: "Invalid type for: ROBLOX_BADGE_ID"}).nullable().parse(process.env.ROBLOX_BADGE_ID) : null,
 
     BLOXLINK_API_KEY: process.env.BLOXLINK_API_KEY ? z.coerce.string().nullable().parse(process.env.BLOXLINK_API_KEY) : null,
-
     BLOXLINK_API_BASE_URL: z.coerce.string().default("https://api.blox.link/v4/").parse(process.env.BLOXLINK_API_BASE_URL),
     
 };
