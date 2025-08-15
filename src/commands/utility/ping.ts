bb5200f3d68c570ebbd3cb982bb9611ae3020adf
Tix <tix.personal@gmail.com>
2025-08-16 01:55:27 +0700

    ephemeral

--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -1,13 +1,19 @@
-import { SlashCommandBuilder, type Interaction } from "discord.js";
+import { ApplicationIntegrationType, InteractionContextType, MessageFlags, SlashCommandBuilder, type Interaction } from "discord.js";
 
 const command = new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with pong!")
+    .setContexts(InteractionContextType.BotDM, InteractionContextType.Guild, InteractionContextType.PrivateChannel)
+    .setIntegrationTypes(ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall)
 
 async function execute(interaction: Interaction) {
     if (!interaction.isChatInputCommand()) return;
 
-    await interaction.reply('Pong!');
+    await interaction.reply({
+        flags: [MessageFlags.Ephemeral],
+        content: "Success.",
+    });
+    await interaction.user.send("Pong!");
 }
 
 export { command, execute };
